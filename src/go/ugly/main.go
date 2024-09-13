9fff03709408f46ef4aa65d655319ae5d98fbf45
Bryan T. Richardson <bryan@activeshadow.com>
2024-09-13 10:08:31 -0600

    support VNC connections

--- src/go/ugly/main.go
+++ src/go/ugly/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"phenix/api/experiment"
 	"phenix/api/vm"
 	"phenix/store"
+	"phenix/util/mm"
 	"phenix/util/plog"
+	"phenix/web/util"
 
 	"github.com/gorilla/mux"
+	"github.com/mitchellh/mapstructure"
+	"golang.org/x/net/websocket"
 )
 
-var basePath string
+var (
+	basePath  string
+	assetPath = "/opt/phenix/ugly"
+)
 
 func main() {
 	plog.NewPhenixHandler()
@@ -21,11 +30,13 @@ func main() {
 
 	switch len(os.Args) {
 	case 2:
-		tmplPath = os.Args[1]
+		assetPath = os.Args[1]
 	case 3:
-		tmplPath = os.Args[1]
+		assetPath = os.Args[1]
 		basePath = os.Args[2]
 	}
+
+	tmplPath = assetPath + "/templates"
 
 	plog.Debug("UI config", "templates", tmplPath, "base URL", basePath, "port", 3001)
 
@@ -36,6 +47,10 @@ func main() {
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, basePath+"/", http.StatusSeeOther)
 	})
+
+	router.PathPrefix("/novnc/").Handler(
+		http.FileServer(http.Dir(assetPath + "/public")),
+	)
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -87,6 +102,70 @@ func main() {
 		}
 
 		http.Redirect(w, r, fmt.Sprintf("%s/?experiment=%s&vm-filter=%s", basePath, exp, r.FormValue("vm-filter")), http.StatusSeeOther)
+	})
+
+	// GET /experiments/{exp}/vms/{name}/vnc
+	router.HandleFunc("/experiments/{exp}/vms/{name}/vnc", func(w http.ResponseWriter, r *http.Request) {
+		plog.Debug("HTTP handler called", "handler", "GetVNC")
+
+		var (
+			vars = mux.Vars(r)
+			exp  = vars["exp"]
+			name = vars["name"]
+		)
+
+		vm, err := vm.Get(exp, name)
+		if err != nil {
+			http.Error(w, "VM not found", http.StatusNotFound)
+			return
+		}
+
+		config := newVNCBannerConfig(exp, name)
+
+		if banner, ok := vm.Annotations["vncBanner"]; ok {
+			switch banner := banner.(type) {
+			case string:
+				config.finalize(banner)
+			case map[string]interface{}:
+				if err := mapstructure.Decode(banner, &config); err != nil {
+					plog.Error("decoding vncBanner annotation for VM", "vm", name, "err", err)
+				} else {
+					config.finalize()
+				}
+			default:
+				plog.Error("unexpected interface type for vncBanner annotation")
+			}
+		} else {
+			config.finalize(fmt.Sprintf("EXP: %s - VM: %s", exp, name))
+		}
+
+		// set no-cache headers
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate") // HTTP 1.1.
+		w.Header().Set("Pragma", "no-cache")                                   // HTTP 1.0.
+		w.Header().Set("Expires", "0")                                         // Proxies.
+
+		tmpl := template.Must(template.New("vnc.html").ParseFiles(filepath.Join(tmplPath, "vnc.html")))
+		tmpl.Execute(w, config)
+	})
+
+	// GET /experiments/{exp}/vms/{name}/vnc/ws
+	router.HandleFunc("/experiments/{exp}/vms/{name}/vnc/ws", func(w http.ResponseWriter, r *http.Request) {
+		plog.Debug("HTTP handler called", "handler", "GetVNCWebSocket")
+
+		var (
+			vars = mux.Vars(r)
+			exp  = vars["exp"]
+			name = vars["name"]
+		)
+
+		endpoint, err := mm.GetVNCEndpoint(mm.NS(exp), mm.VMName(name))
+		if err != nil {
+			plog.Error("getting VNC endpoint", "err", err)
+			http.Error(w, "", http.StatusBadRequest)
+			return
+		}
+
+		websocket.Handler(util.ConnectWSHandler(endpoint)).ServeHTTP(w, r)
 	})
 
 	if err := http.ListenAndServe(":3001", router); err != nil {
